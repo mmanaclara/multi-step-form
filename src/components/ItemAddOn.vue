a811136d832652cf9f860bb5601dba4d2824192f
Clara Moreira <anacacamm@gmail.com>
2023-02-23 08:51:22 -0300

    create add-ons page

---
+++ src/components/ItemAddOn.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="addon--wrapper">
+    <input type="checkbox" v-model="addon" :id="service" />
+    <div class="flex flex-col mr-auto">
+      <label for="service">{{ label }}</label>
+      <span>{{ description }}</span>
+    </div>
+    <p v-if="(hasMonthlyPrice = true)">+${{ priceMonthly }}/mo</p>
+    <p v-if="hasYearlyPrice">+${{ priceYearly }}/yr</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  label: String,
+  description: String,
+  service: String,
+  priceMonthly: Number,
+  priceYearly: Number,
+});
+
+const addon = ref("");
+</script>
+
+<style lang="scss" scoped>
+input {
+  width: 20px;
+  height: 20px;
+}
+
+input[type="checkbox"] {
+  accent-color: #504aae;
+}
+
+label {
+  color: #022a56;
+  font-weight: 500;
+}
+
+span {
+  font-size: 0.875rem;
+  color: #9699ab;
+}
+
+p {
+  color: #504aae;
+  font-size: 0.875rem;
+}
+</style>
--- src/views/PickAddOns.vue
+++ src/views/PickAddOns.vue
@@ -1,7 +1,63 @@
 <template>
-  <div>
-    <h1>This is Step 3</h1>
+  <div class="router--wrapper">
+    <header>
+      <h2 class="title">Pick Add-ons</h2>
+      <span class="description"
+        >Add-ons help enhance your gaming experience.</span
+      >
+    </header>
+
+    <form class="main--wrapper">
+      <div class="flex flex-col gap-4">
+        <ItemAddOn
+          v-for="item in services"
+          :key="item.id"
+          :label="item.label"
+          :description="item.description"
+          :priceMonthly="item.priceMonthly"
+          :priceYearly="item.priceYearly"
+          :service="item.service"
+        />
+      </div>
+
+      <footer>
+        <RouterLink to="/selectyourplan">Go Back</RouterLink>
+        <button class="confirm--button">Confirm</button>
+      </footer>
+    </form>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+import ItemAddOn from "../components/ItemAddOn.vue";
+
+const services = [
+  {
+    id: 1,
+    label: "Online service",
+    service: "online",
+    description: "Access to multiplayer games",
+    priceMonthly: 1,
+    priceYearly: 10,
+  },
+  {
+    id: 2,
+    label: "Larger storage",
+    service: "storage",
+    description: "Extra 1TB of cloud save",
+    priceMonthly: 2,
+    priceYearly: 20,
+  },
+  {
+    id: 3,
+    label: "Customizable profile",
+    service: "profile",
+    description: "Custom theme on your profile",
+    priceMonthly: 2,
+    priceYearly: 20,
+  },
+];
+</script>
+
+<style scoped></style>
